08d013cbf3fb2291865432f1c35bd4b9a44ea0ec
danyel66 <danielonyeka247@gmail.com>
2020-03-03 01:28:23 +0100

    fourth commit

--- MarkoWebsite/settings.py
+++ MarkoWebsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'jh8*ld%e#r+(gr5qv8ykq=^!x&-#rhmt$gc(vywxa*ca)=!^i0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['markoweb.herokuapp.com']
+ALLOWED_HOSTS = ['markoweb.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
