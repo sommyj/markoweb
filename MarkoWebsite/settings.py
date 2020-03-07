1b22d936aa8065bf837b1e9c30f63283470904d9
danyel66 <danielonyeka247@gmail.com>
2020-03-07 13:31:36 +0100

    ssl config1

--- MarkoWebsite/settings.py
+++ MarkoWebsite/settings.py
@@ -14,8 +14,8 @@ import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
