077a174d140303b8e7f4d220a9132e9bae0f6df4
danyel66 <danielonyeka247@gmail.com>
2020-03-24 12:16:34 +0100

    django.sites2

--- MarkoWebsite/settings.py
+++ MarkoWebsite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jh8*ld%e#r+(gr5qv8ykq=^!x&-#rhmt$gc(vywxa*ca)=!^i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://markoventures.com', 'www.markoventures.com', 'markoweb.herokuapp.com', '127.0.0.1']
 
