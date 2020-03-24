3afb347d9bf07190b5aea5f9742ad9b753a9a67c
danyel66 <danielonyeka247@gmail.com>
2020-03-24 13:21:06 +0100

    modify database

--- MarkoWebsite/settings.py
+++ MarkoWebsite/settings.py
@@ -26,8 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'jh8*ld%e#r+(gr5qv8ykq=^!x&-#rhmt$gc(vywxa*ca)=!^i0'
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://markoventures.com', 'www.markoventures.com', 'markoweb.herokuapp.com', '127.0.0.1']
 
