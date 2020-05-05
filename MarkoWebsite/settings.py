2c79769f31c5fa4b1fdc954471ac166acd2df8af
danyel66 <danielonyeka247@gmail.com>
2020-05-05 06:11:46 +0100

    debug mode2

--- MarkoWebsite/settings.py
+++ MarkoWebsite/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = (os.environ.get('DEBUG_VALUE', '') != 'False')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['https://markoventures.com', 'www.markoventures.com', 'markoweb.herokuapp.com', '127.0.0.1']
 
