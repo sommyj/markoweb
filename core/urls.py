1ecfa85c95f3fb3c010adaaf56a895b89ccf3b97
danyel66 <danielonyeka247@gmail.com>
2020-05-20 22:13:09 +0100

    our business page

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, about, contact, CategoryView, ItemDetailView
+from .views import index, about, business, contact, CategoryView, ItemDetailView
 
 
 app_name = 'core'
@@ -8,7 +8,7 @@ app_name = 'core'
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
-    path('business/', about, name='business'),
+    path('business/', business, name='business'),
     path('contact/', contact, name='contact'),
     path('category/', CategoryView.as_view(), name='category'),
     path('product/<slug>/', ItemDetailView.as_view(),
